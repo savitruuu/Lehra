36aefe073f1badd9625c6713de7bfc5bb918d627
savitruuuu <savitrugadagkar@gmail.com>
2026-09-06 12:30:05 +0530

    Update Raag Hemant dhrut lehra notes

--- Non-MERN/audio.js
+++ Non-MERN/audio.js
@@ -572,7 +572,7 @@ class LehraAudioEngine {
     }
 
     if (window._activeRaagKey === "hemant" && this.bpm >= 190) {
-      const drutNotes = [12, null, 12, null, 12, null, 10, 12, 10, null, null, 9, 4, 5, 9, 10];
+      const drutNotes = [12, null, 12, null, 12, null, 16, 17, 14, null, 12, null, 11, 12, 9, 11];
       return drutNotes[index % drutNotes.length];
     }
 
